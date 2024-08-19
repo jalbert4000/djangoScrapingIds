accce28c55473a70c13252c37eb9123036f8805a
jorge <jalbert400@gmail.com>
2024-08-19 10:49:56 -0500

    se ha modificado el archivo settings

--- djangoScrapingIds/settings.py
+++ djangoScrapingIds/settings.py
@@ -117,14 +117,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
-#STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR / 'static']
 #STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 BASE_URL="https://django-scraping-ids.vercel.app"
@@ -132,5 +130,8 @@ BASE_URL="https://django-scraping-ids.vercel.app"
 MEDIA_URL = "/media/"
 #MEDIA_ROOT = os.path.join(BASE_DIR, 'imgs/')
 MEDIA_ROOT = BASE_DIR / "imgs"
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
